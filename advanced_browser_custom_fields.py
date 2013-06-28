fc766f7dae2e9105ccdb93b7fab6c140b22bca29
Houssam Salem <houssam.salem.au@gmail.com>
2013-06-28 20:00:29 +1000

    Version bump (0.1alpha4)

--- advanced_browser_custom_fields.py
+++ advanced_browser_custom_fields.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Version: 0.1alpha3
+# Version: 0.1alpha4
 # See github page to report issues or to contribute:
 # https://github.com/hssm/advanced-browser
 
--- internal_fields.py
+++ internal_fields.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+# Version: 0.1alpha4
+# See github page to report issues or to contribute:
+# https://github.com/hssm/advanced-browser
+
 from anki.hooks import addHook
 import advanced_browser
 
