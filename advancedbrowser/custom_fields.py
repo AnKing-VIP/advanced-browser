deb8ad544c5716082362851981bffaf84aeac5c3
Houssam Salem <houssam.salem.au@gmail.com>
2013-07-28 23:50:55 +1000

    Update custom_fields to use the new approach as well.

--- advancedbrowser/custom_fields.py
+++ advancedbrowser/custom_fields.py
@@ -68,15 +68,31 @@ class CustomFields:
         # this collection to decide how to build the context menu.
         self.customColumns = []
 
+    def buildKnownModels(self):
+        for model in mw.col.models.all():
+            # For some reason, some mids return as unicode, so convert to int
+            mid = int(model['id'])
+            # And some platforms get a signed 32-bit integer from SQlite, so we
+            # will also provide an index to that as a workaround.
+            mid32 = (mid + 2**31) % 2**32 - 2**31
+            self.modelFieldPos[mid] = {}
+            self.modelFieldPos[mid32] = {}
+            for field in model['flds']:
+                name = field['name']
+                ord = field['ord']
+                type = "_field_"+name #prefix to avoid potential clashes
+                self.modelFieldPos[mid][name] = ord
+                self.modelFieldPos[mid32][name] = ord
+                if type not in self.fieldTypes: #avoid dupes
+                    self.fieldTypes[type] = name
     
     def onAdvBrowserLoad(self, advBrowser):
         """Called when the Advanced Browser add-on has finished
         loading. Create and add all custom columns owned by this
         add-on."""
 
-
         # Create a new SQL function that we can use in our queries.
-        mw.col.db._db.create_function("valueForField", 3, valueForField)
+        mw.col.db._db.create_function("valueForField", 3, self.valueForField)
                
         # First review
         def cFirstOnData(c, n, t):
@@ -85,12 +101,13 @@ class CustomFields:
             if first:
                 return time.strftime("%Y-%m-%d", time.localtime(first / 1000))
        
-        _customColumns.append(CustomColumn(
+        cc = advBrowser.newCustomColumn(
             type = 'cfirst',
             name = 'First Review',
             onData = cFirstOnData,
             onSort = lambda: "(select min(id) from revlog where cid = c.id)"
-        ))
+        )
+        self.customColumns.append(cc)
     
         
         # Last review
@@ -100,12 +117,13 @@ class CustomFields:
             if last:
                 return time.strftime("%Y-%m-%d", time.localtime(last / 1000))
        
-        _customColumns.append(CustomColumn(
+        cc = advBrowser.newCustomColumn(
             type = 'clast',
             name = 'Last Review',
             onData = cLastOnData,
             onSort = lambda: "(select max(id) from revlog where cid = c.id)"
-        ))
+        )
+        self.customColumns.append(cc)
     
         
         # Average time
@@ -115,12 +133,13 @@ class CustomFields:
             if avgtime:
                 return str(round(avgtime / 1000, 1)) + "s"
         
-        _customColumns.append(CustomColumn(
+        cc = advBrowser.newCustomColumn(
             type = 'cavgtime',
             name = 'Time (Average)',
             onData = cAvgtimeOnData,
             onSort = lambda: "(select avg(time) from revlog where cid = c.id)"
-        ))    
+        )
+        self.customColumns.append(cc)
     
     
         # Total time
@@ -130,49 +149,49 @@ class CustomFields:
             if tottime:
                 return str(round(tottime / 1000, 1)) + "s"
     
-        _customColumns.append(CustomColumn(
+        cc = advBrowser.newCustomColumn(
             type = 'ctottime',
             name = 'Time (Total)',
             onData = cTottimeOnDAta,
             onSort = lambda: "(select sum(time) from revlog where cid = c.id)"
-        ))
-    
+        )
+        self.customColumns.append(cc)
+        
         
         # Tags
-        _customColumns.append(CustomColumn(
+        cc = advBrowser.newCustomColumn(
             type = 'ntags',
             name = 'Tags',
             onData = lambda c, n, t: " ".join(unicode(tag) for tag in n.tags),
             onSort = lambda: "n.tags"
-        ))
-    
+        )
+        self.customColumns.append(cc)
         
         # Note fields
-        buildKnownModels()
+        self.buildKnownModels()
         
         def fldOnData(c, n, t):
-            field = _fieldTypes[t]
+            field = self.fieldTypes[t]
             if field in c.note().keys():
                 #return anki.utils.stripHTML(c.note()[field])
                 return stripHTML(c.note()[field])
     
         def getOnSort(f): return lambda: f
         
-        for type, name in _fieldTypes.iteritems():
+        for type, name in self.fieldTypes.iteritems():
             srt = ("(select valueForField(mid, flds, '%s') "
                    "from notes where id = c.nid)" % name)
             
-            _customColumns.append(CustomColumn(
+            cc = advBrowser.newCustomColumn(
                 type = type,
                 name = name,
                 onData = fldOnData,
                 onSort = getOnSort(srt)
-            ))
-        
-        for column in _customColumns:
-            advanced_browser.addCustomColumn(column)
+            )
+            self.customColumns.append(cc)
 
-    def onBuildContextMenu(self, menu):
+
+    def onBuildContextMenu(self, contextMenu):
         """Build our part of the browser columns context menu. Decide
         which columns to show.
         
@@ -181,74 +200,46 @@ class CustomFields:
         """
         
         # Model might have changed. Ensure we only offer existing columns.
-        buildKnownModels()
+        self.buildKnownModels()
         
-        fldGroup = advanced_browser.ContextMenuGroup("Fields")
-        for column in _customColumns:
-            if column.type in _fieldTypes:
+        fldGroup = contextMenu.newSubMenu("Fields")
+        for column in self.customColumns:
+            if column.type in self.fieldTypes:
                 fldGroup.addItem(column)
             else:
-                advanced_browser.addContextItem(column)
+                contextMenu.addItem(column)
+
+
+    def valueForField(self, mid, flds, fldName):
+        """
+        Function called from SQLite to get the value of a field, given a
+        field name and the model id for the note.
         
-        advanced_browser.addContextItem(fldGroup)
-
-def valueForField(mid, flds, fldName):
-    """
-    Function called from SQLite to get the value of a field, given a
-    field name and the model id for the note.
+        mid is the model id. The model contains the definition of a note,
+        including the names of all fields.
+        
+        flds contains the text of all fields, delimited by the character
+        "x1f". We split this and index into it according to a precomputed
+        index for the model (mid) and field name (fldName).
+        
+        fldName is the name of the field we are after.
+        """
     
-    mid is the model id. The model contains the definition of a note,
-    including the names of all fields.
-    
-    flds contains the text of all fields, delimited by the character
-    "x1f". We split this and index into it according to a precomputed
-    index for the model (mid) and field name (fldName).
-    
-    fldName is the name of the field we are after.
-    """
-
-    try:
-        index = _modelFieldPos.get(mid).get(fldName, None)
-        if index is not None:
-            fieldsList = flds.split("\x1f", index+1)
-            #return anki.utils.stripHTML(fieldsList[index])
-            return stripHTML(fieldsList[index])
-    except Exception, e:
-        print "Failed to get value for field."
-        print "Mid:" + (mid or 'None')
-        print "flds" + (flds or 'None')
-        print "fldName" + (fldName or 'None')
-        print "_modelFieldPos" + _modelFieldPos
-        print "Error was: " + e.message
+        try:
+            index = self.modelFieldPos.get(mid).get(fldName, None)
+            if index is not None:
+                fieldsList = flds.split("\x1f", index+1)
+                #return anki.utils.stripHTML(fieldsList[index])
+                return stripHTML(fieldsList[index])
+        except Exception, e:
+            print "Failed to get value for field."
+            print "Mid:" + (mid or 'None')
+            print "flds" + (flds or 'None')
+            print "fldName" + (fldName or 'None')
+            print "_modelFieldPos" + self.modelFieldPos
+            print "Error was: " + e.message
 
 
-def buildKnownModels():
-    global _fieldTypes
-    global _modelFieldPos
-
-    for model in mw.col.models.all():
-        # For some reason, some mids return as unicode, so convert to int
-        mid = int(model['id'])
-        # And some platforms get a signed 32-bit integer from SQlite, so we
-        # will also provide an index to that as a workaround.
-        mid32 = (mid + 2**31) % 2**32 - 2**31
-        _modelFieldPos[mid] = {}
-        _modelFieldPos[mid32] = {}
-        for field in model['flds']:
-            name = field['name']
-            ord = field['ord']
-            type = "_field_"+name #prefix to avoid potential clashes
-            _modelFieldPos[mid][name] = ord
-            _modelFieldPos[mid32][name] = ord
-            if type not in _fieldTypes: #avoid dupes
-                _fieldTypes[type] = name
-
-
-
-    
-
-
-#cf = CustomFields()
-
-#addHook("advBrowserLoaded", cf.onAdvBrowserLoad)
-#addHook("advBrowserBuildContext", cf.onBuildContextMenu)
+cf = CustomFields()
+addHook("advBrowserLoaded", cf.onAdvBrowserLoad)
+addHook("advBrowserBuildContext", cf.onBuildContextMenu)
