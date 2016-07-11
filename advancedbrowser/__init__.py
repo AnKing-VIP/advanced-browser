55af396560c383493c9f99acfdbbd11cb8632548
Houssam Salem <houssam.salem.au@gmail.com>
2016-07-11 15:13:55 +1000

    Remove upgrade code
    No longer valid for anyone using this version

--- advancedbrowser/__init__.py
+++ advancedbrowser/__init__.py
@@ -3,8 +3,6 @@
 # See github page to report issues or to contribute:
 # https://github.com/hssm/advanced-browser
 
-from anki.hooks import addHook
-from aqt import mw
 from advancedbrowser.core import AdvancedBrowser
 
 # Advanced Browser modules
@@ -12,28 +10,4 @@ from advancedbrowser import custom_fields
 from advancedbrowser import note_fields
 
 # Uncomment the next line to include internal fields (card/note/model IDs, etc)
-from advancedbrowser import internal_fields
-
-def onLoad():
-    # Move saved filters to built-in setting
-    transferSavedFilters()
-
-def transferSavedFilters():
-    """Courtesy upgrade of saved filter setting. Move them out of this
-    add-on and into the built-in setting."""
-    
-    d = mw.pm.profile.get('ab_saved_filters', {})
-    if d:
-        # Add conf if missing (i.e., first run)
-        if not mw.col.conf.has_key('savedFilters'):
-            mw.col.conf['savedFilters'] = {}
-
-        # Transfer filters to collection conf
-        for key, value in d.iteritems():
-            mw.col.conf['savedFilters'][key] = value
-        
-        # Remove local conf
-        mw.pm.profile.pop('ab_saved_filters')
-
-# Do any important work when the collection loads.
-addHook("profileLoaded", onLoad)
+#from advancedbrowser import internal_fields
