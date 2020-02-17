10b56ed49ef60abe9232c57c1e2789622ee5a29a
Arthur Milchior <Arthur@Milchior.fr>
2020-02-17 04:52:15 -0800

    Show selected card when opened from the browser
    I assure that if the current card is in the search result, it is
    shown.

--- advancedbrowser/advancedbrowser/core.py
+++ advancedbrowser/advancedbrowser/core.py
@@ -126,12 +126,22 @@ class AdvancedDataModel(DataModel):
     def one_card_by_note(self):
         nids = set()
         filtered_card = []
+        selected_card = self.browser.mw.reviewer.card
+        selected_cid = selected_card.id if selected_card else 0
+        selected_nid = selected_card.nid if selected_card else 0
+        idx_of_selected_note = None
+        # position of the unique card of note currently in reviewer
         for cid in self.cards:
             card = self.browser.mw.col.getCard(cid)
             nid = card.nid
             if nid not in nids:
                 filtered_card.append(cid)
                 nids.add(nid)
+                if nid == selected_nid:
+                    idx_of_selected_note = len(filtered_card) - 1
+            elif cid == selected_cid:  # nid in nids
+                filtered_card.pop(idx_of_selected_note)
+                filtered_card.append(cid)
         self.cards = filtered_card
 
     def myFindCards(self, query, order):
