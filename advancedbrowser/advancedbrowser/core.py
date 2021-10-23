020e5147e5bb86ce288d7c5ade5473a9528adecc
vwkd <33468089+vwkd@users.noreply.github.com>
2021-10-23 15:26:41 +0300

    left-align

--- advancedbrowser/advancedbrowser/core.py
+++ advancedbrowser/advancedbrowser/core.py
@@ -90,7 +90,7 @@ class AdvancedBrowser:
                 notes_mode_label=column.name,
                 sorting=BrowserColumns.SORTING_NORMAL if column.onSort() else BrowserColumns.SORTING_NONE,
                 uses_cell_font=False,
-                alignment=BrowserColumns.ALIGNMENT_CENTER,
+                alignment=BrowserColumns.ALIGNMENT_START,
             )
 
     def willSearch(self, ctx: SearchContext):
