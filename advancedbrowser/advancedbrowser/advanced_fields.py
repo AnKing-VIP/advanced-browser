e34e01d0460f1aa24ace51040d7f1c3b4a8444ab
ijgnd <does_not_exist@example.com>
2022-09-28 12:16:30 +0200

    Revert "Fix sorting by overdue interval"
    This reverts commit fc03f5e6d1a7d7596961604774fb32416bd8f599.

--- advancedbrowser/advancedbrowser/advanced_fields.py
+++ advancedbrowser/advancedbrowser/advanced_fields.py
@@ -145,9 +145,8 @@ class AdvancedFields:
             if val:
                 return mw.col.format_timespan(val * 24 * 60 * 60, context=FormatTimeSpanContext.INTERVALS)
 
-
         srt = (f"""
-        (select
+        select
           (case
              when odid then null
              when queue = {QUEUE_TYPE_LRN} then null
@@ -155,9 +154,8 @@ class AdvancedFields:
              when type = {CARD_TYPE_NEW} then null
              when {mw.col.sched.today} - due <= 0 then null
              when (queue = {QUEUE_TYPE_REV} or queue = {QUEUE_TYPE_DAY_LEARN_RELEARN} or (type = {CARD_TYPE_REV} and queue < 0)) then ({mw.col.sched.today} - due)
-           end
           )
-        from cards where id = c.id) asc nulls last""")
+        where id = c.id asc nulls last""")
 
         cc = advBrowser.newCustomColumn(
             type='coverdueivl',
